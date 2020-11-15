eb7ee8de2ca72808bff6080f4e82c29bff72dd75
Cristian Dean <cristian.dean@corp.globo.com>
2020-11-15 14:00:27 -0300

    fix post-process all collector command

--- csv-collector/cmd/process_all.go
+++ csv-collector/cmd/process_all.go
@@ -11,12 +11,9 @@ var postProcessAllCmd = &cobra.Command{
 	Use:   "process-all",
 	Short: "Imports the data from CSV and post-processes it",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if err := importCSVFilesCmd.Execute(); err != nil {
+		if err := importCSVFiles(); err != nil {
 			return err
 		}
-		if err := postProcessAll(); err != nil {
-			return err
-		}
-		return nil
+		return postProcessAll()
 	},
 }
